76933c292121f7103493898402b236d2e128db45
Humberto Tello <betoelo2@gmail.com>
2020-06-21 21:12:08 -0300

    Desafio 2 bugs consertados

--- desafio2/scripts.js
+++ desafio2/scripts.js
@@ -1,11 +1,17 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
+console.log(cards)
 
 for (let card of cards) {
   card.addEventListener("click", function() {
     const recipeImg = card.querySelector("img").src
+    const title = card.querySelector(".card__title p").textContent
+    const author = card.querySelector(".card__author p").textContent
+
     modalOverlay.classList.add("active")
     modalOverlay.querySelector("img").src = recipeImg
+    modalOverlay.querySelector(".modal__title").innerHTML = title
+    modalOverlay.querySelector(".modal__author").innerHTML = author
   })
 }
 
